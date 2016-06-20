c57b09f79916804baa238f2031c5c3980b718565
JurassicSPb <Jurassic5@yandex.ru>
2016-06-20 21:34:51 +0300

    changes

--- src/PhoneBook/Main.java
+++ src/PhoneBook/Main.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
  * Created by Юрий on 11.06.2016.
  */
 public class Main {
-    public static void main (String [] args)
-    {
-        System.out.println ("Телефонная книга.");
-        Scanner sc = new Scanner (System.in);
+    public static void main (String [] args) {
+        System.out.println("Телефонная книга.");
+        Scanner sc = new Scanner(System.in);
         ArrayList<DetailContact> list = new ArrayList<>();
         while (true) {
             System.out.println("Menu: \n 1. Добавить контакт \n 2. Показать все контакты \n 3. Выход \n 4. Удалить контакт");
@@ -18,18 +17,14 @@ public class Main {
                 System.out.println("Введите имя: ");
                 book.setName(sc.next());
                 while (true) {
-                    System.out.println("Введите  телефон: ");
-                    System.out.println("Нажмите 'y' чтобы продолжить или 'n' для выхода");
+                    System.out.println("Введите  телефон или нажмите 'n' для выхода: ");
                     String str = sc.next();
-                    String y = "y";
                     String n = "n";
                     if (str.equals(n))
                         break;
-                    else if (str.equals(y))
-                        break;
                     else
                         book.setPhone(str);
-                    }
+                }
                 System.out.println("Введите email: ");
                 book.setEmail(sc.next());
                 System.out.println("Введите адрес: ");
@@ -37,8 +32,7 @@ public class Main {
                 System.out.println("Введите место работы: ");
                 book.setWorkplace(sc.next());
                 list.add(book);
-            }
-            else if (i == 2) {
+            } else if (i == 2) {
                 if (list.size() == 0) {
                     System.out.println("Телефонная книга пустая.");
                     continue;
@@ -55,11 +49,21 @@ public class Main {
                     }
                 }
                 for (int j = 0; j < list.size(); j++) {
-                       System.out.println(list.get(j).getName() + " " + list.get(j).getPhone() + " " + list.get(j).getEmail() +
-                                " " + list.get(j).getAddress() + " " + list.get(j).getWorkplace());
-
+                    if (list.get(j).getPhone().size() == 1) {
+                        System.out.println(list.get(j).getName() + ", " + list.get(j).getPhone() + ", " + list.get(j).getEmail() +
+                                ", " + list.get(j).getAddress() + " " + list.get(j).getWorkplace() + "\n");
+                    }
+                    else {
+                        System.out.print("\n" + list.get(j).getName() + ", ");
+                        System.out.print(list.get(j).getEmail() + ", " + list.get(j).getAddress() + ", " + list.get(j).getWorkplace() + "\n");
+                        System.out.println("<--- phones --->");
+                        for (int k = 0; k < list.get(j).getPhone().size(); k++) {
+                            System.out.printf("%s \n", list.get(j).getPhone().get(k));
+                        }
+                        System.out.printf("<<< %s phones >>> \n", list.get(j).getName());
+                        System.out.println();
+                    }
                 }
-
             }
                 else if (i == 3) {
                     System.out.println("Выход из программы");
